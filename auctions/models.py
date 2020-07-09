160f30affca230261d7fc153119735e870c77193
Adrià Sala <adriasala124@gmail.com>
2020-07-09 10:24:03 +0200

    Created models: listing, bid, comment

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,21 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+
+    name = models.CharField(max_length=64)
+    base_price = models.DecimalField(decimal_places=2)
+    description = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
+
+class Bid(models.Model):
+
+    price = models.DecimalField(decimal_places=2)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bids")
+
+class Comment(models.Model):
+
+    text = models.TextField()
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
