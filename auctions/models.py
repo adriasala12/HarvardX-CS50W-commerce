144e155ea536ca189aa447bfa03779b59051246e
Adrià Sala <adriasala124@gmail.com>
2020-07-09 19:49:51 +0200

    Bid now keeps track of the user who made it

--- auctions/models.py
+++ auctions/models.py
@@ -11,12 +11,13 @@ class Listing(models.Model):
     base_price = models.DecimalField(max_digits=8, decimal_places=2)
     description = models.TextField()
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
-    image_url = models.CharField(max_length=200, default="https://freesvg.org/img/Placeholder.png")
+    image_url = models.CharField(max_length=600, default="https://freesvg.org/img/Placeholder.png")
 
 class Bid(models.Model):
 
     price = models.DecimalField(max_digits=8, decimal_places=2)
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bids")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Comment(models.Model):
 
