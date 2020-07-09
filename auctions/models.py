a49496b77aa7fba940e9d7a24c0b27951c8e9212
Adrià Sala <adriasala124@gmail.com>
2020-07-09 11:24:18 +0200

    Reordered model classes

--- auctions/models.py
+++ auctions/models.py
@@ -5,6 +5,10 @@ from django.db import models
 class User(AbstractUser):
     pass
 
+class Category(models.Model):
+
+    name = models.CharField(max_length=64)
+
 class Listing(models.Model):
 
     name = models.CharField(max_length=64)
@@ -23,7 +27,3 @@ class Comment(models.Model):
     text = models.TextField()
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="comments")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-
-class Category(models.Model):
-
-    name = models.CharField(max_length=64)
