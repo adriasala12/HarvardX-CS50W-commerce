66e184444e4a0d593f9ca969fac2a29a396943a7
Adrià Sala <adriasala124@gmail.com>
2020-07-09 16:31:42 +0200

    Added image url for listings

--- auctions/models.py
+++ auctions/models.py
@@ -11,6 +11,7 @@ class Listing(models.Model):
     base_price = models.DecimalField(max_digits=8, decimal_places=2)
     description = models.TextField()
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
+    image_url = models.CharField(max_length=200)
 
 class Bid(models.Model):
 
