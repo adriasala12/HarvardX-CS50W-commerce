49e669f5e83bd2bc818c9979e4f489372f48a053
Adrià Sala <adriasala124@gmail.com>
2020-07-13 22:30:45 +0200

    Added categories to listings

--- auctions/models.py
+++ auctions/models.py
@@ -14,6 +14,27 @@ class Listing(models.Model):
     image_url = models.CharField(max_length=600, default="https://freesvg.org/img/Placeholder.png")
     is_active = models.BooleanField(default=True)
 
+    CATEGORIES = [
+        ("1", "Books"),
+        ("2", "Business & Industrial"),
+        ("3", "Clothing, Shoes & Accessories"),
+        ("4", "Collectibles"),
+        ("5", "Consumer Electronics"),
+        ("6", "Crafts"),
+        ("7", "Dolls & Bears"),
+        ("8", "Home & Garden"),
+        ("9", "Motors"),
+        ("10", "Pet Supplies"),
+        ("11", "Sporting Goods"),
+        ("12", "Sports Mem, Cards & Fan Shop"),
+        ("13", "Toys & Hobbies"),
+        ("14", "Antiques"),
+        ("15", "Computers, Tablets & Networking")
+    ]
+
+    category = models.CharField(max_length=2, choices=CATEGORIES)
+
+
 class Bid(models.Model):
 
     price = models.DecimalField(max_digits=8, decimal_places=2)
